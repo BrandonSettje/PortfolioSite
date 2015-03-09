37df77f53dea616683f1bacf2205694f3cb51f92
Brandon Settje <brandon.settje@gmail.com>
2015-03-09 01:58:30 -0600

    Portfolio
    Base layout and images

---
+++ BSettjeDeveloperSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSettjeDeveloperSite.MvcApplication" Language="C#" %>
---
+++ BSettjeDeveloperSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BSettjeDeveloperSite.Startup))]
+namespace BSettjeDeveloperSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
